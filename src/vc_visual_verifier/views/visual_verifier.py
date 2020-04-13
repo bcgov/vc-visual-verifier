f7e158ea3391c4763631cce301666cd72cd48ad6
Emiliano Suñé <emiliano.sune@gmail.com>
2020-04-13 10:26:19 -0700

    Add claim value to verifier view
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- src/vc_visual_verifier/views/visual_verifier.py
+++ src/vc_visual_verifier/views/visual_verifier.py
@@ -2,13 +2,21 @@ from django.conf import settings
 from django.shortcuts import render
 
 
-def verifier(request):    
+def verifier(request):
     required_claims = settings.OIDC_CLAIMS_REQUIRED.split(",")
     claims = []
     if not request.user.is_anonymous:
         for claim in required_claims:
             if claim in request.user.oidc_user.userinfo:
-                claims.append({ "name": claim, "provided": True  })
+                claims.append(
+                    {
+                        "name": claim,
+                        "value": request.user.oidc_user.userinfo[claim],
+                        "provided": True,
+                    }
+                )
             else:
-                claims.append({ "name": claim, "provided": False  })
-    return render(request, "visual_verifier.html", {"settings": settings, "claims": claims})
+                claims.append({"name": claim, "provided": False})
+    return render(
+        request, "visual_verifier.html", {"settings": settings, "claims": claims}
+    )
