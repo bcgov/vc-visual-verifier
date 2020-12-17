6a0c2572d321ebd346f97ff006dc3e7ffa165999
Emiliano Suñé <emiliano.sune@gmail.com>
2020-12-17 14:18:29 -0800

    Enable whitenoise static file server middleware
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- src/vc_visual_verifier/settings.py
+++ src/vc_visual_verifier/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -134,13 +135,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+if DEBUG:
+    # serve static files from development server
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    # serve static files using production webserver
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
-STATICFILES_DIRS = [
-    BASE_DIR + "/static",
-]
 
 
 # Settings for django-oidc-rp
